c6db9496f701b50f119f1d909ebca17dcc9c3a0d
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 01:36:54 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Redirect the root URL to the tasks index page
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
+// Resourceful routes for tasks, excluding create and show actions
 Route::resource('tasks', 'App\Http\Controllers\TaskController')->except(['create', 'show']);
+
+// Route for updating task order
 Route::put('tasks/reorder', 'App\Http\Controllers\TaskController@reorder')->name('tasks.reorder');
 
+// Resourceful routes for projects
 Route::resource('projects', 'App\Http\Controllers\ProjectController');

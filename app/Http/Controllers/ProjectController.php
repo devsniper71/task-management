a8911f79f9b63cedc6e827295f3fe5c4deb4bacd
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-27 23:41:43 +0600

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,68 +7,58 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $projects = Project::all();
-
         $projectId = request()->get('project');
+
         if ($projectId) {
-            $tasks = Project::findOrFail($projectId)->tasks;
+            $project = Project::findOrFail($projectId);
+            $tasks = $project->tasks;
         } else {
+            $project = null;
             $tasks = [];
         }
 
-        return view('projects.index', compact('projects', 'tasks'));
+        return view('projects.index', compact('projects', 'project', 'tasks'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('projects.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+        ]);
+
+        Project::create($validatedData);
+
+        return redirect()->route('projects.index')->with('success', 'Project created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Project $project)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Project $project)
     {
-        //
+        return view('projects.edit', compact('project'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Project $project)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+        ]);
+
+        $project->update($validatedData);
+
+        return redirect()->route('projects.index')->with('success', 'Project updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('projects.index')->with('success', 'Project deleted successfully.');
     }
 }

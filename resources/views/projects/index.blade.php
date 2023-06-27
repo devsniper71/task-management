2dbda4f36e3e3022167e5827555eeafeb519fa3b
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 01:09:50 +0600

    Update index.blade.php

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,27 +1,40 @@
-<h1>Projects</h1>
+@extends('layouts.app')
 
-@if ($projects->count() > 0)
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="{{ route('projects.index', ['project' => $project->id]) }}">{{ $project->name }}</a>
-                <a href="{{ route('projects.edit', $project->id) }}">Edit</a>
-                <form action="{{ route('projects.destroy', $project->id) }}" method="POST"
-                      style="display: inline-block;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('Are you sure you want to delete this project?')">
-                        Delete
-                    </button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
-@else
-    <p>No projects found.</p>
-@endif
+@section('content')
+    <div class="container">
+        <h1>Projects</h1>
+        <div class="card">
+            <div class="card-body">
+                @if ($projects->count() > 0)
+                    <ul class="list-group">
+                        @foreach ($projects as $project)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    {{ $project->name }}
+                                </div>
+                                <div>
+                                    <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                                    <form action="{{ route('projects.destroy', $project->id) }}" method="POST"
+                                          style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm"
+                                                onclick="return confirm('Are you sure you want to delete this project?')">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    <p>No projects found.</p>
+                @endif
+            </div>
+        </div>
 
-<hr>
+        <hr>
 
-<a href="{{ route('projects.create') }}">Create Project</a>
-<a href="{{ route('tasks.index') }}">Create Task</a>
+        <a href="{{ route('projects.create') }}" class="btn btn-primary">Create Project</a>
+    </div>
+@endsection

201cbe6e6b96f1159caf27eaafb4d1cec6a5319d
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 01:09:47 +0600

    Update create.blade.php

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,12 +1,24 @@
-<h1>Create Project</h1>
+@extends('layouts.app')
 
-<form action="{{ route('projects.store') }}" method="POST">
-    @csrf
+@section('content')
+    <div class="container">
+        <h1>Create Project</h1>
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('projects.store') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Project Name:</label>
+                        <input type="text" name="name" id="name" class="form-control" required>
+                    </div>
 
-    <div>
-        <label for="name">Project Name:</label>
-        <input type="text" name="name" id="name" required>
+                    <hr>
+
+                    <button type="submit" class="btn btn-primary">Create Project</button>
+                </form>
+            </div>
+        </div>
+
+        <a href="{{ route('projects.index') }}" class="btn btn-secondary mt-3">Back</a>
     </div>
-
-    <button type="submit">Create Project</button>
-</form>
+@endsection

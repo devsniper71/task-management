ddbb7ee2a1e26485e308cb00fcfbfff8c34fbe15
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 01:34:45 +0600

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
@@ -9,35 +10,47 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the tasks.
+     * Display a listing of tasks.
      *
      * @return \Illuminate\View\View
      */
     public function index()
     {
+        // Get tasks ordered by priority
         $tasks = Task::orderBy('priority')->get();
+        // Get all projects
         $projects = Project::all();
 
+        // Return view with tasks and projects data
         return view('tasks.index', compact('tasks', 'projects'));
+    }
+
+    /**
+     * Show the form for creating a new task.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        // Get all projects
+        $projects = Project::all();
+
+        // Return view with projects data
+        return view('tasks.create', compact('projects'));
     }
 
     /**
      * Store a newly created task in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\TaskRequest  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'priority' => 'required|integer|min:1',
-            'project_id' => 'required|exists:projects,id',
-        ]);
+        // Create a new task with the validated data
+        $task = Task::create($request->validated());
 
-        $task = new Task($validatedData);
-        $task->save();
-
+        // Redirect to the index page with a success message
         return redirect()->route('tasks.index')->with('success', 'Task created successfully.');
     }
 
@@ -49,28 +62,26 @@ class TaskController extends Controller
      */
     public function edit(Task $task)
     {
+        // Get all projects
         $projects = Project::all();
 
+        // Return view with task and projects data
         return view('tasks.edit', compact('task', 'projects'));
     }
 
     /**
      * Update the specified task in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\TaskRequest  $request
      * @param  \App\Models\Task  $task
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Task $task)
+    public function update(TaskRequest $request, Task $task)
     {
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'priority' => 'required|integer|min:1',
-            'project_id' => 'required|exists:projects,id',
-        ]);
+        // Update the task with the validated data
+        $task->update($request->validated());
 
-        $task->update($validatedData);
-
+        // Redirect to the index page with a success message
         return redirect()->route('tasks.index')->with('success', 'Task updated successfully.');
     }
 
@@ -82,25 +93,30 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
+        // Delete the task
         $task->delete();
 
+        // Redirect to the index page with a success message
         return redirect()->route('tasks.index')->with('success', 'Task deleted successfully.');
     }
 
     /**
-     * Reorder the tasks based on the provided task IDs.
+     * Reorder the tasks based on the given task IDs.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function reorder(Request $request)
     {
+        // Get the task IDs from the request input
         $taskIds = $request->input('taskIds', []);
 
+        // Update the priority of tasks based on the given order
         foreach ($taskIds as $index => $taskId) {
             Task::where('id', $taskId)->update(['priority' => $index + 1]);
         }
 
+        // Return a JSON response indicating success
         return response()->json(['success' => true]);
     }
 }

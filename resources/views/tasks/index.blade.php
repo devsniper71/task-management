711c4579076605846bb186d2bff8d1812a2ac99b
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 00:59:44 +0600

    Update index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,66 +1,78 @@
-<h1>Tasks</h1>
+@extends('layouts.app')
 
-@if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+@section('content')
+    <div class="container">
+        <h1>Tasks</h1>
 
-<h2>Create Task</h2>
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-<form action="{{ route('tasks.store') }}" method="POST">
-    @csrf
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Create Task</h2>
 
-    <div>
-        <label for="name">Task Name:</label>
-        <input type="text" name="name" id="name" required>
-    </div>
-
-    <div>
-        <label for="priority">Priority:</label>
-        <input type="number" name="priority" id="priority" required>
-    </div>
-
-    <div>
-        <label for="project_id">Project:</label>
-        <select name="project_id" id="project_id">
-            <option value="">None</option>
-            @foreach ($projects as $project)
-                <option value="{{ $project->id }}">{{ $project->name }}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <button type="submit">Create Task</button>
-</form>
-
-<hr>
-
-<h2>Existing Tasks</h2>
-
-@if ($tasks->count() > 0)
-    <ul id="task-list">
-        @foreach ($tasks as $task)
-            <li data-task-id="{{ $task->id }}">
-                {{ $task->name }} (Priority: {{ $task->priority }}) - {{ $task->project->name }}
-                <a href="{{ route('tasks.edit', $task->id) }}">Edit</a>
-                <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: inline-block;">
+                <form action="{{ route('tasks.store') }}" method="POST">
                     @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('Are you sure you want to delete this task?')">
-                        Delete
-                    </button>
+
+                    <div class="form-group">
+                        <label for="name">Task Name:</label>
+                        <input type="text" name="name" id="name" class="form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="priority">Priority:</label>
+                        <input type="number" name="priority" id="priority" class="form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="project_id">Project:</label>
+                        <select name="project_id" id="project_id" class="form-control">
+                            <option value="">None</option>
+                            @foreach ($projects as $project)
+                                <option value="{{ $project->id }}">{{ $project->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <hr>
+
+                    <button type="submit" class="btn btn-primary">Create Task</button>
                 </form>
-            </li>
-        @endforeach
-    </ul>
-@else
-    <p>No tasks found.</p>
-@endif
+            </div>
 
-<hr>
+            <div class="col-md-6">
+                <h2>Existing Tasks</h2>
 
-<a href="{{ route('projects.index') }}">Projects</a>
+                @if ($tasks->count() > 0)
+                    <ul id="task-list" class="list-group">
+                        @foreach ($tasks as $task)
+                            <li class="list-group-item" data-task-id="{{ $task->id }}">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        {{ $task->name }} (Priority: {{ $task->priority }}) - {{ $task->project->name }}
+                                    </div>
+                                    <div>
+                                        <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: inline-block;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this task?')">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    <p>No tasks found.</p>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
 
 @push('scripts')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>

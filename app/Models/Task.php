05b3b83b35976545d990e803615cadd51e71eabc
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 00:37:47 +0600

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'priority',
+        'project_id',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }

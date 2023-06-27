ac012b11bdccd018a97f8d31072271cc648c4ccd
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-28 02:33:23 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,25 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Task Management App</title>
     <!-- Add the Bootstrap CDN link below -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        .content {
+            flex: 1;
+        }
+
+        .footer {
+            background-color: #f8f9fa;
+            padding: 20px;
+            text-align: center;
+        }
+    </style>
     @stack('styles')
 </head>
 <body>
@@ -30,10 +48,20 @@
     </div>
 </nav>
 
-<div class="container mt-4">
+<div class="container mt-4 content">
     @yield('content')
 </div>
 
+<footer class="footer">
+    &copy; {{ date('Y') }} Task Management App. All rights reserved. Created by <a href="https://github.com/raselism71"
+                                                                                   target="_blank">Md
+        Rasel Ahmed</a>
+</footer>
+
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 @stack('scripts')
 </body>
 </html>
